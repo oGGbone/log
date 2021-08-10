ab8d098cf967a2fb7bc853e0b41dd7bba7120c3d
www <wennewww@163.com>
2021-08-10 23:03:04 +0800

    默认更改列表

---
+++ web/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+
+class Job_Account(models.Model):
+    User_account = models.CharField(max_length=30,unique=True)
+    User_password = models.CharField(max_length=20)
+    User_name = models.CharField(max_length=20)
+    User_number = models.CharField(max_length=20,unique=True)
+    User_post = models.CharField(max_length=10,unique=True)
+# Create your models here.
+class Job_Log(models.Model):
+    flag = ((1,'已完成任务'),(2,'主任务'),(3,'子任务'),(4,'下阶段任务'))
+    a = 1
+    level = []
+    while a <= 20:
+        b = str(int(a * 0.05 * 100)) + '%'
+        level.append((a,b))
+        a += 1
+    level = tuple (level)
+    Job_useraccount = models.CharField(max_length=30,unique=True)
+    Job_flag = models.IntegerField(choices=flag)
+    Job_name = models.CharField(max_length=30,unique=True)
+    Job_finish_date = models.IntegerField()
+    Job_begin_date = models.IntegerField()
+    Job_over_date = models.IntegerField()
+    Job_level = models.IntegerField(choices=level)
+    Job_introduce = models.TextField()
+
+class Job_Milepost(models.Model):
+    Milepost_date = models.IntegerField()
+    Milepost_content = models.TextField()
+
+class Job_Log_Main(models.Model):
+    a = 1
+    level = []
+    while a <= 20:
+        b = str(int(a * 0.05 * 100)) + '%'
+        level.append((a, b))
+        a += 1
+    level = tuple(level)
+    Main_useraccount = models.CharField(max_length=30,unique=True)
+    Main_name = models.CharField(max_length=30,unique=True)
+    Main_finish_date = models.IntegerField()
+    Main_level = models.IntegerField(choices=level)
+    Main_introduce = models.TextField()
+
+class Job_Log_Secondly(models.Model):
+    a = 1
+    level = []
+    while a <= 20:
+        b = str(int(a * 0.05 * 100)) + '%'
+        level.append((a, b))
+        a += 1
+    level = tuple(level)
+    Secondly_useraccount = models.CharField(max_length=30,unique=True)
+    Secondly_name = models.CharField(max_length=30,unique=True)
+    Secondly_finish_date = models.IntegerField()
+    Secondly_level = models.IntegerField(choices=level)
+    Secondly_introduce = models.TextField()
+
+class Job_Log_Next(models.Model):
+    Next_useraccount = models.CharField(max_length=30,unique=True)
+    Next_name = models.CharField(max_length=30,unique=True)
+    Next_begin_date = models.IntegerField()
+    Next_introduce = models.TextField()
---
+++ web/views.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+from django.contrib import auth
+from django.shortcuts import render,redirect
+import random,hashlib
+from web import models
+from django.views.decorators import csrf
+from django.http import HttpRequest
+
+def login(request):
+    if request.COOKIES.get('user'):
+        return redirect('/index/')
+    if request.method == 'GET':
+        return render(request,'in.html')
+    username = request.POST.get('user')
+    password = request.POST.get('passwd')
+    user_obj = models.Job_Account.objects.filter(User_account=username,User_password=password).first()
+#    ---------密码加密
+#    cookie_random = str(random.random())
+#    cookie_md5 = hashlib.md5()
+#    cookie_md5.update(cookie_random.encode(encoding='utf-8'))
+#    cookie = cookie_md5.hexdigest()
+    #   print (user_obj.User_account)
+#   ----------
+    if not user_obj:
+        return render(request,'in.html',{'filed':'用户名或密码有问题。'})
+    else:
+        rep = redirect('/index/')
+        rep.set_cookie('user',username,)
+        return rep
+
+
+def index(request):
+#    print(request.COOKIES.get('is_login'))
+    status = request.COOKIES.get('user')
+    if not status:
+        return redirect('/in/')
+    else:
+        Account = models.Job_Account.objects.all()
+    return render(request,'index.html',{'User_name':Account,'User_number':Account,'User_account':Account,'User_post':Account})
+
+def logout(request):
+    rep = redirect('/in/')
+    rep.delete_cookie('user')
+    return rep
+
+def passwd_change(request):
+    if request.method == 'GET':
+        return render(request,'passwd_change.html')
+    status = request.COOKIES.get('user')
+    if not status:
+        return redirect('/in/')
+    password = request.POST.get('password')
+    change = models.Job_Account.objects.get(User_account=status)
+    change.User_password = password
+    change.save()
+    return render(request,'passwd_change.html',{'password_change':'密码修改成功'})
+
+def Log(request,account):
+    status = request.COOKIES.get('user')
+    if not status:
+        return redirect('/in/')
+    else:
+        job_main = models.Job_Log_Main.objects.filter(Main_useraccount = account)
+        job_secondly = models.Job_Log_Secondly.filter(Secoundly_useraccount = account)
+        job_next = models.Job_Log_Next.filter(Next_useraccount = account)
+        return render(request,'log.html',{'useraccount':job_main})
+######
+#123
+######
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
